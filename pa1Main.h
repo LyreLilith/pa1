40d930d22b96eeac0d86528a63bd42440cac9129
Lilith Sutton <lilithr.sutton@gmail.com>
2023-09-06 20:19:52 -0700

    Commititng the first part of PA one that include a limited number of headers

---
+++ pa1Main.h
@@ -0,0 +1,16 @@
+#ifndef PA1MAIN
+#define PA1Main
+
+
+/*
+* @breif: Prints the menu for the matching  game
+* @parameters: void
+* @output: void
+*/
+void print_Menu();
+
+/*
+* @breif: initualizes the items of a csv to a link list of a specific format 
+* @parameters: csv  file
+* 
+
