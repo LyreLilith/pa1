d658a8a8112651161363f14da0243e1b51e42d2a
Lilith Sutton <lilithr.sutton@gmail.com>
2023-09-06 23:24:02 -0700

    Added additional functions to mainheader, might remove to be more oop

--- pa1Main.h
+++ pa1Main.h
@@ -12,5 +12,16 @@ void print_Menu();
 /*
 * @breif: initualizes the items of a csv to a link list of a specific format 
 * @parameters: csv  file
-* 
+* @output: void
+*/ 
+void  makeList(ifstream matches);
+
+/*
+* @breif:  makes a  new node and alots memory for it
+* @parameters:	 string answeer
+* @parameters:   string command
+* @output: node* newNode
+*/
+void makeNode(string answer, string command);
+
 
