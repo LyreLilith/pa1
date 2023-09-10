1a563e832de6025d03512f8dd22d9ba47182e62a
Lilith Sutton <lilithr.sutton@gmail.com>
2023-09-10 16:57:38 -0700

    Added the header for list.h

--- pa1/list.h
+++ pa1/list.h
@@ -1,4 +1,21 @@
 #ifndef LIST_H
 #define LIST_H
+#include <std.io>
+#include "Node.h"
 
+template<typename T1, typename T2>
+class List {
+public:
+    List();  
+    ~List(); 
+
+    void insertAtFront(T1 value1, T2 value2);
+    Node<T1, T2> getHead();
+    int getSize();
+    void deleteList();
+
+private:
+    Node<T1, T2>* Head;
+    int size;
+}
 #endif
